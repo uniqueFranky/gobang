b6b4a6f49ca230a56209fb500054c86c4279e3ff
uniqueFranky <y_r_b@qq.com>
2022-12-15 22:24:21 +0800

    add put piece

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import boardImg from './images/board.jpg';
+import blackImg from './images/black.png';
+import whiteImg from './images/white.png';
+
 
 class Cell extends React.Component {
+
     render() {
         const i = this.props.rowId;
         const j = this.props.colId;
-        return <area onClick={() => this.onclick()} shape={'circle'} coords={(37.5 + j * 49) + ',' + (37.5 + i * 48) + ',10'} />;
+        let pieceStyle = {
+            position: 'absolute',
+            top: (37.5 + i * 48 - 15).toString() + 'px',
+            left: (37 + j * 49 - 12.5).toString() + 'px',
+            height: '50px',
+            width: '50px'
+        };
+        if(j <= 1) {
+            pieceStyle["left"] = (37 + j * 49 - 14).toString() + 'px';
+        } else if(j <= 3) {
+            pieceStyle["left"] = (37 + j * 49 - 15).toString() + 'px';
+        } else if(j >= 11) {
+            pieceStyle["left"] = (37 + j * 49 - 9).toString() + 'px';
+        } else {
+            pieceStyle["left"] = (37 + j * 49 - 12  ).toString() + 'px';
+        }
+
+        if(i <= 7) {
+            pieceStyle["top"] = (37.5 + i * 48 - 17).toString() + 'px';
+        }
+        if(this.props.selector === 'None') {
+            return <area onClick={() => this.onclick()} shape={'circle'} coords={(37.5 + j * 49) + ',' + (37.5 + i * 48) + ',10'} alt={'map'}/>;
+        } else if(this.props.selector === 'Player') {
+            return <img className={'piece'} src={blackImg} alt={'black'} style={pieceStyle}/>;
+        } else {
+            return <img className={'piece'} src={whiteImg} alt={'white'} style={pieceStyle}/>;
+        }
     }
     onclick() {
         const i = this.props.rowId;
@@ -29,15 +59,27 @@ class Board extends React.Component {
     render() {
 
         let areas = new Array(15 * 15);
+        let pieces = new Array(15 * 15);
         for(let i = 0; i < 15; i++) {
             for(let j = 0; j < 15; j++) {
-                areas[i * 15 + j] = <Cell key={(i * 15 + j).toString()} rowId={i} colId={j} didClick={this.didClick} />;
+                if(this.props.status[i][j] === 'None') {
+                    areas[i * 15 + j] = <Cell key={(i * 15 + j).toString()} rowId={i} colId={j} selector={this.props.status[i][j]} didClick={this.didClick} />;
+                } else {
+                   pieces[i * 15 + j] = <Cell key={(i * 15 + j).toString()} rowId={i} colId={j} selector={this.props.status[i][j]} didClick={this.didClick} />;
+                }
             }
         }
-        console.log(areas);
+        const piecesStyle = {
+            position: 'absolute',
+            top: '0',
+            left: '0'
+        };
         return (
             <div className={'board'}>
-                <img width={768} height={746.25} src={boardImg} alt={'board'} useMap={'#boardMap'}/>
+                <img width={768} height={746.25} src={boardImg} alt={'board'} useMap={'#boardMap'} />
+                <div className={'pieces'} style={piecesStyle}>
+                    {pieces}
+                </div>
                 <map name={'boardMap'}>
                     {areas}
                 </map>
@@ -54,7 +96,7 @@ class Game extends React.Component {
             stat[i] = new Array(15).fill('None');
         }
         this.state = {
-            status: this.stat,
+            status: stat,
             nextPlayer: 'Player',
         };
         this.didClick = this.didClick.bind(this);
@@ -62,10 +104,19 @@ class Game extends React.Component {
 
     didClick(i, j) {
         console.log(i, j);
+        if(this.state.status[i][j] === 'None') {
+            let newStatus = this.state.status.slice();
+            newStatus[i][j] = this.state.nextPlayer;
+            this.setState({
+                status: newStatus,
+                nextPlayer: this.state.nextPlayer === 'Player' ? 'Computer' : 'Player'
+            });
+        }
     }
 
     render() {
-        return <Board didClick={this.didClick}/>;
+        console.log(this.state.status);
+        return <Board didClick={this.didClick} status={this.state.status}/>;
     }
 }
 
