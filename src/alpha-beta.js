bace8940f49e256b9e5cde7f7b1547eff8194c98
uniqueFranky <y_r_b@qq.com>
2022-12-16 14:35:15 +0800

    add alpha-beta, but seems to have some bugs

---
+++ src/alpha-beta.js
@@ -0,0 +1,169 @@
+
+
+export function aiSolve(status, callback) {
+    let ij = alpha_beta(status, 1, 3, -(10**11), 10**11);
+    callback(ij[0], ij[1]);
+}
+
+function alpha_beta(status, curDep, maxDep, alpha, beta) {
+    // console.log("dep=" + curDep + " alpha=" + alpha + " beta=" + beta);
+    if(curDep === maxDep) {
+        // console.log("score=" + calcScore(status));
+        return calcScore(status);
+    }
+    let maxposi, maxposj;
+    if(curDep % 2 === 0) { // Player's turn, min node
+        for(let i = 0; i < 15; i++) {
+            for(let j = 0; j < 15; j++) {
+                if(status[i][j] === 'None') {
+                    status[i][j] = 'Player';
+                    let val = alpha_beta(status, curDep + 1, maxDep, -(10**11), alpha);
+                    alpha = alpha < val ? alpha : val;
+                    status[i][j] = 'None';
+                    if(alpha <= beta) {
+                        // console.log("cut min alpha=" + alpha + " beta=" + beta);
+                        return alpha;
+                    }
+                }
+            }
+        }
+        return alpha;
+    } else { // Computer's turn, max node
+        for(let i = 0; i < 15; i++) {
+            for(let j = 0; j < 15; j++) {
+                if(status[i][j] === 'None') {
+                    status[i][j] = 'Computer';
+                    let val = alpha_beta(status, curDep + 1, maxDep, 10**11, alpha);
+                    if(val > alpha) {
+                        alpha = val;
+                        maxposi = i;
+                        maxposj = j;
+                    }
+                    status[i][j] = 'None';
+                    if(alpha >= beta) {
+                        // console.log("cut max alpha=" + alpha + " beta=" + beta);
+                        return alpha;
+                    }
+                }
+            }
+        }
+    }
+    // console.log(alpha);
+    if(curDep === 1) {
+        return [maxposi, maxposj];
+    } else {
+        return alpha;
+    }
+
+}
+
+function calc(status, turn) {
+    let totScore = 0;
+    let opp;
+    if(turn === 'Player') {
+        opp = 'Computer';
+    } else {
+        opp = 'Player';
+    }
+
+    for(let i = 0; i < 15; i++) {
+        for(let j = 0; j < 11; j++) {
+            let selfNum = 0, oppNum = 0;
+            for(let k = j; k < j + 5; k++) {
+                if(status[i][k] === turn) {
+                    selfNum++;
+                } else if(status[i][k] === opp) {
+                    oppNum++;
+                }
+            }
+            if(oppNum === 0) {
+                totScore += 10 ** (selfNum === 5 ? 10 : selfNum);
+            }
+        }
+    }
+
+    for(let j = 0; j < 15; j++) {
+        for(let i = 0; i < 11; i++) {
+            let selfNum = 0, oppNum = 0;
+            for(let k = i; k < i + 5; k++) {
+                if(status[k][j] === turn) {
+                    selfNum++;
+                } else if(status[k][j] === opp) {
+                    oppNum++;
+                }
+            }
+            if(oppNum === 0) {
+                totScore += 10 ** (selfNum === 5 ? 10 : selfNum);
+            }
+        }
+    }
+
+    for(let dec = 10; dec > -1; dec--) {
+        for(let i = dec; i < 11; i++) {
+            let selfNum = 0, oppNum = 0;
+            for(let k = 0; k < 5; k++) {
+                if(status[i + k][i - dec + k] === turn) {
+                    selfNum++;
+                } else if(status[i + k][i - dec + k] === opp) {
+                    oppNum++;
+                }
+            }
+            if(oppNum === 0) {
+                totScore += 10 ** (selfNum === 5 ? 10 : selfNum);
+            }
+        }
+        if(dec > 0) {
+            for(let j = dec; j < 11; j++) {
+                let selfNum = 0, oppNum = 0;
+                for(let k = 0; k < 5; k++) {
+                    if(status[j - dec + k][j + k] === turn) {
+                        selfNum++;
+                    } else if(status[j - dec + k][j + k]=== opp) {
+                        oppNum++;
+                    }
+                }
+                if(oppNum === 0) {
+                    totScore += 10 ** (selfNum === 5 ? 10 : selfNum);
+                }
+            }
+        }
+    }
+
+    for(let sum = 4; sum < 15; sum++) {
+        for(let i = 4; i <= sum; i++) {
+            let selfNum = 0, oppNum = 0;
+            for(let k = 0; k < 5; k++) {
+                if(status[i - k][sum - i + k] === turn) {
+                    selfNum++;
+                } else if(status[i - k][sum - i + k] === opp) {
+                    oppNum++;
+                }
+            }
+            if(oppNum === 0) {
+                totScore += 10 ** (selfNum === 5 ? 10 : selfNum);
+            }
+        }
+
+        for(let j = 4; j <= sum; j++) {
+            let selfNum = 0, oppNum = 0;
+            for(let k = 0; k < 5; k++) {
+                if(status[sum - j + k][j - k] === turn) {
+                    selfNum++;
+                } else if(status[sum - j + k][j - k] === turn) {
+                    oppNum++;
+                }
+            }
+            if(oppNum === 0) {
+                totScore += 10 ** (selfNum === 5 ? 10 : selfNum);
+            }
+        }
+
+    }
+
+    return totScore;
+}
+
+
+function calcScore(status) {
+    return calc(status, 'Computer') - calc(status, 'Player');
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import boardImg from './images/board.jpg';
 import blackImg from './images/black.png';
 import whiteImg from './images/white.png';
+import {aiSolve} from "./alpha-beta";
 
 
 class Cell extends React.Component {
@@ -20,7 +21,7 @@ class Cell extends React.Component {
         if(j <= 1) {
             pieceStyle["left"] = (37 + j * 49 - 14).toString() + 'px';
         } else if(j <= 3) {
-            pieceStyle["left"] = (37 + j * 49 - 15).toString() + 'px';
+            pieceStyle["left"] = (37 + j * 49 - 17).toString() + 'px';
         } else if(j >= 11) {
             pieceStyle["left"] = (37 + j * 49 - 9).toString() + 'px';
         } else {
@@ -29,9 +30,11 @@ class Cell extends React.Component {
 
         if(i <= 7) {
             pieceStyle["top"] = (37.5 + i * 48 - 17).toString() + 'px';
+        } else {
+            pieceStyle["top"] = (37.5 + i * 48 - 19).toString() + 'px';
         }
         if(this.props.selector === 'None') {
-            return <area onClick={() => this.onclick()} shape={'circle'} coords={(37.5 + j * 49) + ',' + (37.5 + i * 48) + ',10'} alt={'map'}/>;
+            return <area onClick={() => this.onclick()} shape={'circle'} coords={(37.5 + j * 49) + ',' + (37.5 + i * 48) + ',12'} alt={'map'}/>;
         } else if(this.props.selector === 'Player') {
             return <img className={'piece'} src={blackImg} alt={'black'} style={pieceStyle}/>;
         } else {
@@ -89,6 +92,8 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+
     constructor(props) {
         super(props);
         let stat = new Array(15);
@@ -100,25 +105,166 @@ class Game extends React.Component {
             nextPlayer: 'Player',
         };
         this.didClick = this.didClick.bind(this);
+        this.aiCallback = this.aiCallback.bind(this);
     }
 
     didClick(i, j) {
         console.log(i, j);
-        if(this.state.status[i][j] === 'None') {
-            let newStatus = this.state.status.slice();
-            newStatus[i][j] = this.state.nextPlayer;
-            this.setState({
-                status: newStatus,
-                nextPlayer: this.state.nextPlayer === 'Player' ? 'Computer' : 'Player'
-            });
+        if(this.state.nextPlayer === 'Player') {
+            if(this.state.status[i][j] === 'None') {
+                let newStatus = this.state.status.slice();
+                newStatus[i][j] = 'Player';
+                this.setState({
+                    status: newStatus,
+                    nextPlayer: 'Computer'
+                });
+
+                aiSolve(this.state.status, this.aiCallback);
+            } else {
+                alert("该格已被下过棋子");
+            }
+        } else {
+            alert("现在是AI的回合");
         }
     }
 
+    aiCallback(i, j) {
+        let newStatus = this.state.status.slice();
+        newStatus[i][j] = 'Computer';
+        this.setState({
+            status: newStatus,
+            nextPlayer: 'Player'
+        });
+    }
+
+    checkWinner() {
+        for(let i = 0; i < 15; i++) {
+            for(let j = 0; j < 11; j++) {
+                let playerNum = 0, computerNum = 0;
+                for(let k = j; k < j + 5; k++) {
+                    if(this.state.status[i][k] === 'Computer') {
+                        computerNum++;
+                    } else if(this.state.status[i][k] === 'Player') {
+                        playerNum++;
+                    }
+                }
+                if(computerNum === 5) {
+                    return 'Computer';
+                } else if(playerNum === 5) {
+                    return 'Player';
+                }
+            }
+        }
+
+        for(let j = 0; j < 15; j++) {
+            for(let i = 0; i < 11; i++) {
+                let playerNum = 0, computerNum = 0;
+                for(let k = i; k < i + 5; k++) {
+                    if(this.state.status[k][j] === 'Computer') {
+                        computerNum++;
+                    } else if(this.state.status[k][j] === 'Player') {
+                        playerNum++;
+                    }
+                }
+                if(computerNum === 5) {
+                    return 'Computer';
+                } else if(playerNum === 5) {
+                    return 'Player';
+                }
+            }
+        }
+
+        for(let dec = 10; dec > -1; dec--) {
+            for(let i = dec; i < 11; i++) {
+                let playerNum = 0, computerNum = 0;
+                for(let k = 0; k < 5; k++) {
+                    if(this.state.status[i + k][i - dec + k] === 'Computer') {
+                        computerNum++;
+                    } else if(this.state.status[i + k][i - dec + k] === 'Player') {
+                        playerNum++;
+                    }
+                }
+                if(computerNum === 5) {
+                    return 'Computer';
+                } else if(playerNum === 5) {
+                    return 'Player';
+                }
+            }
+            if(dec > 0) {
+                for(let j = dec; j < 11; j++) {
+                    let playerNum = 0, computerNum = 0;
+                    for(let k = 0; k < 5; k++) {
+                        if(this.state.status[j - dec + k][j + k] === 'Computer') {
+                            computerNum++;
+                        } else if(this.state.status[j - dec + k][j + k]=== 'Player') {
+                            playerNum++;
+                        }
+                    }
+                    if(computerNum === 5) {
+                        return 'Computer';
+                    } else if(playerNum === 5) {
+                        return 'Player';
+                    }
+                }
+            }
+        }
+
+        for(let sum = 4; sum < 15; sum++) {
+            for(let i = 4; i <= sum; i++) {
+                let playerNum = 0, computerNum = 0;
+                for(let k = 0; k < 5; k++) {
+                    if(this.state.status[i - k][sum - i + k] === 'Computer') {
+                        computerNum++;
+                    } else if(this.state.status[i - k][sum - i + k] === 'Player') {
+                        playerNum++;
+                    }
+                }
+                if(computerNum === 5) {
+                    return 'Computer';
+                } else if(playerNum === 5) {
+                    return 'Player';
+                }
+            }
+
+            for(let j = 4; j <= sum; j++) {
+                let playerNum = 0, computerNum = 0;
+                for(let k = 0; k < 5; k++) {
+                    if(this.state.status[sum - j + k][j - k] === 'Computer') {
+                        computerNum++;
+                    } else if(this.state.status[sum - j + k][j - k] === 'Player') {
+                        playerNum++;
+                    }
+                }
+                if(computerNum === 5) {
+                    return 'Computer';
+                } else if(playerNum === 5) {
+                    return 'Player';
+                }
+            }
+        }
+        return 'None';
+    }
+
     render() {
-        console.log(this.state.status);
+        let winner = this.checkWinner();
+        if(winner !== 'None') {
+            setTimeout(() => {
+                alert(winner + " wins!");
+                let stat = new Array(15);
+                for(let i = 0; i < 15; i++) {
+                    stat[i] = new Array(15).fill('None');
+                }
+                this.setState({
+                    status: stat,
+                    nextPlayer: 'Player'
+                });
+            }, 1000);
+
+        }
         return <Board didClick={this.didClick} status={this.state.status}/>;
     }
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
